b14932ec20399df9e2c00b46a954f5ff4c215c26
Cristobal Miranda <cristobal.miranda.tt@gmail.com>
2020-05-30 01:13:28 -0400

    ntparser+

---
+++ scripts/NTParser.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Cristobal Miranda, 2020
+//
+
+#ifndef RDFCACHEK2_NTPARSER_HPP
+#define RDFCACHEK2_NTPARSER_HPP
+
+
+class NTParser {
+
+};
+
+
+#endif //RDFCACHEK2_NTPARSER_HPP
--- scripts/index_by_k2tree.cpp
+++ scripts/index_by_k2tree.cpp
@@ -15,19 +15,28 @@ struct parsed_options {
   std::string input_data;
 };
 
+void print_help();
+parsed_options parse_cmline(int argc, char **argv);
+
+
+int main(int argc, char **argv) {
+  auto parsed_cmline = parse_cmline(argc, argv);
+}
+
+
+
 void print_help() {
   std::cout << "--input-format\t(-f)\t\t[NT]\n"
             << "--generate-mapping\t(-g)\t\t(boolean)\n"
             << "--input-data\t(-d)\t\t(string-required)" << std::endl;
 }
-
 parsed_options parse_cmline(int argc, char **argv) {
   const char *const short_options = "f::gd:";
   struct option long_options[] = {
-      {"input-format", optional_argument, nullptr, 'f'},
-      {"generate-mapping", optional_argument, nullptr, 'g'},
-      {"input-data", required_argument, nullptr, 'd'},
-      {nullptr, 0, nullptr, 0}};
+          {"input-format", optional_argument, nullptr, 'f'},
+          {"generate-mapping", optional_argument, nullptr, 'g'},
+          {"input-data", required_argument, nullptr, 'd'},
+          {nullptr, 0, nullptr, 0}};
 
   int opt;
   int opt_index;
@@ -39,29 +48,29 @@ parsed_options parse_cmline(int argc, char **argv) {
   bool has_input = false;
 
   while ((
-      opt = getopt_long(argc, argv, short_options, long_options, &opt_index))) {
+          opt = getopt_long(argc, argv, short_options, long_options, &opt_index))) {
     if (opt == -1) {
       break;
     }
 
     switch (opt) {
-    case 'f':
-      if (std::strncmp(optarg, "NT", 2 * sizeof(char)) == 0) {
-        out.input_format = InputFormat::NT;
-      }
-      break;
-    case 'g':
-      out.generate_mapping = true;
-      break;
-    case 'd':
-      out.input_data = optarg;
-      has_input = true;
-      break;
-    case 'h': // to implement
-    case '?':
-    default:
-      print_help();
-      break;
+      case 'f':
+        if (std::strncmp(optarg, "NT", 2 * sizeof(char)) == 0) {
+          out.input_format = InputFormat::NT;
+        }
+        break;
+      case 'g':
+        out.generate_mapping = true;
+        break;
+      case 'd':
+        out.input_data = optarg;
+        has_input = true;
+        break;
+      case 'h': // to implement
+      case '?':
+      default:
+        print_help();
+        break;
     }
   }
 
@@ -72,8 +81,4 @@ parsed_options parse_cmline(int argc, char **argv) {
   }
 
   return out;
-}
-
-int main(int argc, char **argv) {
-  auto parsed_cmline = parse_cmline(argc, argv);
 }
