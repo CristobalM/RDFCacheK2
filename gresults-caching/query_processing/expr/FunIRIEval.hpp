070127ac7709bfcacb4c78c9018493d356ba06db
Cristobal M <cristobal.miranda.tt@gmail.com>
2021-07-24 21:15:23 -0400

    implement FunctionEval

--- gresults-caching/query_processing/expr/FunIRIEval.hpp
+++ gresults-caching/query_processing/expr/FunIRIEval.hpp
@@ -16,8 +16,6 @@ public:
   std::shared_ptr<TermResource>
   eval_fun_with_term_arguments(TermResource &fun_resource,
                                const ExprEval::row_t &row);
-  std::shared_ptr<TermResource> data_type_cast(ExprDataType type,
-                                               TermResource &resource);
 };
 
 #endif // RDFCACHEK2_FUNIRIEVAL_HPP
--- gresults-caching/query_processing/expr/FunctionEval.hpp
+++ gresults-caching/query_processing/expr/FunctionEval.hpp
@@ -9,6 +9,14 @@
 class FunctionEval : public ExprEval {
 public:
   using ExprEval::ExprEval;
+
+  void init() override;
+  std::shared_ptr<TermResource> eval_resource(const row_t &row) override;
+  void validate() override;
+
+  std::shared_ptr<TermResource>
+  eval_fun_with_term_arguments(TermResource &fun_resource,
+                               const ExprEval::row_t &row);
 };
 
 #endif // RDFCACHEK2_FUNCTIONEVAL_HPP
