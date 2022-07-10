2ae80bd3c231bae5612715771336ac2cb33b7c6a
cm <cm>
2022-07-10 15:13:53 -0400

    duration check on test

--- test/node_ids_dyn_mapper_test.cpp
+++ test/node_ids_dyn_mapper_test.cpp
@@ -11,49 +11,43 @@
 #include <gtest/gtest.h>
 #include <vector>
 using namespace k2cache;
-// TEST(node_ids_dyn_mapper_test, can_map_big_ids_to_smaller) {
-//   auto pcm = basic_pcm();
-//   static constexpr auto total_points = 10000UL;
-//
-//   auto update_config = UpdaterSession::get_initial_update_k2tree_config();
-//   auto predicate_id = 343UL;
-//   K2TreeMixed k2tree(update_config);
-//   K2TreeBulkOp op(k2tree);
-//
-//   for (auto i = 0UL; i < total_points; i++) {
-//     auto j =  i + 1;
-//     op.insert(j, j);
-//   }
-//   auto updates = std::vector<K2TreeUpdates>{
-//       {predicate_id, &k2tree, nullptr},
-//   };
-//   pcm->get_updates_logger().log(updates);
-//   ASSERT_TRUE(pcm->get_updates_logger().has_predicate_stored(predicate_id));
-//
-////  ASSERT_TRUE(
-////      pcm->get_predicates_index_cache().has_predicate_active(predicate_id));
-//  auto f = pcm->get_predicates_index_cache().fetch_k2tree(predicate_id);
-//  ASSERT_TRUE(f.exists());
-//  auto &tree = f.get_mutable();
-//  ASSERT_EQ(tree.size(), total_points);
-//  for (auto i = 0UL; i < total_points; i++) {
-//    auto j =   i + 1;
-//    ASSERT_TRUE(tree.has(j, j));
-//  }
-//}
 
 TEST(node_ids_dyn_mapper_test, can_map_big_ids_to_smaller) {
   auto cache_container = mock_cache_container();
   TestingTaskProcessor tp(*cache_container);
   UpdaterSession session(&tp, cache_container.get());
   static constexpr auto total_points = 10000UL;
-  auto predicate_id = 342UL;
+  const auto base_id = 1L << 33L;
+  const auto predicate_id = base_id + 342UL;
+  const auto predicate_id2 = 342UL;
+  const auto base_id2 = predicate_id + 1;
+  auto start = std::chrono::high_resolution_clock::now();
   for (auto i = 0UL; i < total_points; i++) {
-    auto j = i + 1;
+    auto j = i + 1 + base_id2;
     TripleNodeId triple((NodeId(j)), NodeId(predicate_id), NodeId(j));
     session.add_triple(triple);
   }
   session.commit_updates();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now() - start);
+  std::cout << "first took: " << duration.count() << " ms" << std::endl;
+
+  {
+    UpdaterSession session2(&tp, cache_container.get());
+
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i = 0UL; i < total_points; i++) {
+      auto j = i + 1 + base_id2;
+      TripleNodeId triple((NodeId(j)), NodeId(predicate_id2), NodeId(j));
+      session2.add_triple(triple);
+    }
+    session2.commit_updates();
+    auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::high_resolution_clock::now() - start);
+    std::cout << "second took: " << duration2.count() << " ms" << std::endl;
+    ASSERT_GT(duration, duration2); // the first time should do much more work
+                                    // than the second time
+  }
 
   auto &triple_streamer = tp.create_triples_streamer({predicate_id});
   auto &streamer = triple_streamer.start_streaming_matching_triples(
@@ -66,7 +60,7 @@ TEST(node_ids_dyn_mapper_test, can_map_big_ids_to_smaller) {
             });
   ASSERT_EQ(total_points, streamer_data.size());
   for (auto i = 0UL; i < total_points; i++) {
-    auto j = i + 1;
+    auto j = i + 1 + base_id2;
     ASSERT_EQ(j, streamer_data[i].subject.get_value());
     ASSERT_EQ(j, streamer_data[i].object.get_value());
     ASSERT_EQ(predicate_id, streamer_data[i].predicate.get_value());
